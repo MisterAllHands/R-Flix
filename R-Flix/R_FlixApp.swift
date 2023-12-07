d4b313a2fd076ba861cf00d25b03009ca9e91f27
misterallhands <aldoknowas@gmail.com>
2023-12-07 22:10:35 +0300

    Initial Commit

---
+++ R-Flix/R_FlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  R_FlixApp.swift
+//  R-Flix
+//
+//  Created by Mr Producer on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct R_FlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
